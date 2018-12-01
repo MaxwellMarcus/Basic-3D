f98cf385a9825a18dea117740ba90f5627ae3ac3
bigmaxattack37 <bigmaxmarcus@gmail.com>
2018-11-30 20:21:49 -0600

    no idea what happedning

--- 3DShapes.py
+++ 3DShapes.py
@@ -16,7 +16,13 @@ class _3D:
         self.centerZ = 1000
         self.mouseX = 0
         self.mouseY = 0
+
+        self.keysPressed = []
+
         root.bind('<Motion>',self.mouseSet)
+        root.bind('<KeyPress>',self.keyPressed)
+        root.bind('<KeyRelease>',self.KeyReleased)
+
     def mouseSet(self,event):
         self.mouseX = event.x
         self.mouseY = event.y
@@ -84,20 +90,29 @@ class _3D:
         radiusX = xRad * perspective
         radiusY = yRad * perspective
         shapeMod = [shape[0]*perspective + root.winfo_screenwidth()/2,shape[1]*perspective+root.winfo_screenheight()/2]
-        #canvas.create_oval(shapeMod[0]-radiusX,shapeMod[1]-radiusY,shapeMod[0]+radiusX,shapeMod[1]+radiusY,fill = color)
         return shapeMod
     def project(self,points):
-        for i in range(len(points)-1):
+        for i in range(len(points)):
             p = points[i]
             scale = self.fl/(self.fl + p[2])
             p[3] = p[0] * scale + root.winfo_screenwidth()/2
             p[4] = p[1] * scale + root.winfo_screenheight()/2
         return points
     def drawLines(self,points,indexes):
-        for i in range(len(indexes)-2):
+        for i in range(len(indexes)-1):
             p = points[indexes[i]]
             nextP = points[indexes[i+1]]
             canvas.create_line(p[3],p[4],nextP[3],nextP[4])
+    def translate(self,points,x=0,y=0,z=0):
+        for i in range(len(points)):
+            points[i][0] += x
+            points[i][1] += y
+            points[i][2] += z
+        return points
+    def keyPressed(self,event):
+        self.keysPressed.append(event.keysym)
+    def KeyReleased(self,event):
+        self.keysPressed.remove(event.keysym)
 
 
 _3d = _3D(300)
@@ -116,18 +131,30 @@ rotationSpeed = 1
 baseAngle = 0
 
 points = _3d.project(points)
-while   True:
-    for i in points:
-        canvas.create_rectangle(i[3]-5,i[4]-5,i[3]+5,i[3]+5,fill = 'black')
+
+v = 0
+while v < 1:
+    print(_3d.keysPressed)
+    if 'a' in _3d.keysPressed:
+        _3d.translate(points,x=-2)
+    if 'd' in _3d.keysPressed:
+        _3d.translate(points,x=2)
+    if 'w' in _3d.keysPressed:
+        if 'space' in _3d.keysPressed:_3d.translate(points,z=2)
+        else:_3d.translate(points,y=-2)
+    if 's' in _3d.keysPressed:
+        if 'space' in _3d.keysPressed:_3d.translate(points,z=-2)
+        else:_3d.translate(points,y=2)
+
     canvas.delete(ALL)
 
     points = _3d.project(points)
 
-    '''_3d.drawLines(points,[0,1,2,3,0])
+    _3d.drawLines(points,[0,1,2,3,0])
     _3d.drawLines(points,[4,5,6,7,4])
     _3d.drawLines(points,[0,4])
     _3d.drawLines(points,[1,5])
     _3d.drawLines(points,[2,6])
-    _3d.drawLines(points,[3,7])'''
+    _3d.drawLines(points,[3,7])
 
     root.update()
