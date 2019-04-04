524a200bc681360aacb86df923d8377a48801352
Student <student@nip-mbp-soar-3.local>
2019-04-04 10:53:49 -0500

    the evolution simulator works! Now I need to apply the concept to the neural network

--- evolution_simulator.py
+++ evolution_simulator.py
@@ -18,9 +18,9 @@ class Creature:
         #for i in range(1000):
             #self.x += self.speed
     def mutate(self):
-        change = random.randint(-1,1)
+        change = random.randint(-10,10)
         while self.speed + change < minSpeed or self.speed + change > maxSpeed or change == 0:
-            change = random.randint(-1,1)
+            change = random.randint(-10,10)
         self.speed += change
 def sort(l):
     for i in range(len(l)):
@@ -44,63 +44,64 @@ def sort(l):
         sorted[i] = sorted[i][0]
     return sorted
 
-maxSpeed = 100
+maxSpeed = 1000
 minSpeed = 0
-num_creatures = 15
-creatures = []
-lines = []
-lines2 = []
-lines3 = []
-for i in range(num_creatures):
-    creatures.append(Creature(random.randint(1,100)))
-last = None
-for i in range(15):
-    for l in creatures:
-        l.run()
-    creatures = sort(creatures)
-    half = len(creatures)/2
-    lines.append(creatures[len(creatures)-1].speed)
-    print(half)
-    for i in range(len(creatures)):
-        print(i)
-        if i+1 == int(half):
-            median = creatures[i]
-        if i+1 > half:
-        #    creatures[i] = Creature(creatures[int(i-half)].speed)
-            creatures[i].mutate()
-    lines2.append(creatures[0].speed)
-    lines3.append(median.speed)
-    canvas.delete(ALL)
-    lastX = 0
-    lastY = height/2
-    for i in range(11):
-        canvas.create_line(0,lastY-(i*10)*3,width,lastY-(i*10)*3)
-        canvas.create_text(20,lastY-(i*10)*3,text=str(i*10),font=('TkTextFont',20))
-    for i in range(len(lines)):
-        canvas.create_line(i*width/len(lines),0,i*width/len(lines),height)
-        canvas.create_text()
-    for i in lines:
-        x = lastX + width/len(lines)
-        y = height/2-i*3
-        canvas.create_line(lastX,lastY,x,y,fill = 'red')
-        lastX = x
-        lastY = y
-    lastX = 0
-    lastY = height/2
-    for l in lines2:
-        x = lastX + width/len(lines)
-        y = height/2-l*3
-        canvas.create_line(lastX,lastY,x,y,fill = 'green')
-        lastX = x
-        lastY = y
-    lastX = 0
-    lastY = height/2
-    for l in lines3:
-        x = lastX + width/len(lines)
-        y = height/2-l*3
-        canvas.create_line(lastX,lastY,x,y,fill = 'black')
-        lastX = x
-        lastY = y
+def new_creatures(event):
+    num_creatures = 15
+    creatures = []
+    lines = []
+    lines2 = []
+    lines3 = []
+    for i in range(num_creatures):
+        creatures.append(Creature(random.randint(minSpeed,maxSpeed)))
+    last = None
+    for i in range(15):
+        for l in creatures:
+            l.run()
+        creatures = sort(creatures)
+        half = len(creatures)/2
+        lines.append(creatures[len(creatures)-1].speed)
+        for i in range(len(creatures)):
+            if i+1 == int(half):
+                median = creatures[i]
+            if i+1 > half:
+                creatures[i] = Creature(creatures[int(i-half)].speed)
+                creatures[i].mutate()
+        lines2.append(creatures[0].speed)
+        lines3.append(median.speed)
+        canvas.delete(ALL)
+        lastX = 0
+        lastY = height/2
+        for i in range(11):
+            canvas.create_line(0,lastY-(i*10)*3,width,lastY-(i*10)*3)
+            canvas.create_text(20,lastY-(i*10)*3,text=str(i*maxSpeed/10),font=('TkTextFont',20))
+        for i in range(len(lines)+1):
+            canvas.create_line(i*width/len(lines),0,i*width/len(lines),height-height/2+15)
+            canvas.create_text(i*width/len(lines),height-height/2+10,text=str(i),font=('TkTextFont',20))
+        for i in lines:
+            x = lastX + width/len(lines)
+            y = height/2-(i*3/(maxSpeed/100))
+            canvas.create_line(lastX,lastY,x,y,fill = 'red',width=3)
+            lastX = x
+            lastY = y
+        lastX = 0
+        lastY = height/2
+        for l in lines2:
+            x = lastX + width/len(lines)
+            y = height/2-(l*3/(maxSpeed/100))
+            canvas.create_line(lastX,lastY,x,y,fill = 'green',width=3)
+            lastX = x
+            lastY = y
+        lastX = 0
+        lastY = height/2
+        for l in lines3:
+            x = lastX + width/len(lines)
+            y = height/2-((l*3)/(maxSpeed/100))
+            canvas.create_line(lastX,lastY,x,y,fill = 'black',width=3)
+            lastX = x
+            lastY = y
 
-    root.update()
+        root.update()
+
+root.bind('<Key>',new_creatures)
 root.mainloop()
